43b490877ee1155dc2d4dd82daaff248a8d86317
Frans Allen <git.a@upset.dev>
2020-09-08 06:41:03 +0700

    Correcting Features link

--- views/footer.php
+++ views/footer.php
@@ -6,7 +6,7 @@
     </div>
 
     <a href="https://statically.io/" target="_blank"><?php _e( 'About', 'statically'); ?></a>
-    <a href="https://wordpress.org/plugins/statically/#description" target="_blank"><?php _e( 'Features', 'statically'); ?></a>
+    <a href="https://wordpress.org/plugins/statically/#description-header" target="_blank"><?php _e( 'Features', 'statically'); ?></a>
     <a href="<?php echo admin_url( 'admin.php?page=statically-debugger' ); ?>"><?php _e( 'Debug', 'statically'); ?></a>
 
     <div class="social">
